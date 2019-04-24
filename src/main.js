be4b35d9db6ff1ccb32ccd85b1ae40209c04609a
David Wengrow <dwengrow@gmail.com>
2019-04-23 20:52:00 -0500

    twitter

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
-import twitter from "vue-twitter";
-
+import { Tweet, Moment, Timeline } from "vue-tweet-embed";
 axios.default.baseURL = process.env.NODE_ENV === "development" ? "HTTP://localhost:3000" : "/";
 // hello we will revisit the local host 3000 to update the URL
 
 Vue.config.productionTip = false;
-Vue.use(twitter);
+
 new Vue({
   router,
   render: h => h(App)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,25 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="This is my new app - DAN. Welcome to our app - Lindsey, back end is taking too long!" />
+    <Tweet :id="'759043035355312128'"></Tweet>
+    <Tweet :id="'1119573367961661441'"></Tweet>
+    <Timeline :id="'RealHughJackman'" :sourceType="'profile'" :options="{ tweetLimit: '3' }" />
+    <Timeline :id="'RealHughJackman'" :sourceType="'likes'" :options="{ theme: 'dark' }" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { Tweet } from "vue-tweet-embed";
+import { Timeline } from "vue-tweet-embed";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Tweet,
+    Timeline
   }
 };
 </script>
